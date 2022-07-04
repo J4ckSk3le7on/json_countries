47c1041da005cf3fcfcd200553cf116c75501da1
Jorge <J4ck.DevCode@gmail.com>
2022-07-04 09:09:05 -0500

    Added new countries

---
+++ bank_list.js
@@ -0,0 +1,283 @@
+"guatemala": [
+    {
+        "id": ,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 101,
+        "countryCode": "GT",
+        "name": "Banco de Guatemala"
+    },
+    {
+        "id": ,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 104,
+        "countryCode": "GT",
+        "name": "Crédito Hipotecario Nacional"
+    },
+    {
+        "id": 1,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 112,
+        "countryCode": "GT",
+        "name": "Banco de Los Trabajadores"
+    },
+    {
+        "id": 1,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 113,
+        "countryCode": "GT",
+        "name": "Banco Inmobiliario, S.A."
+    },
+    {
+        "id": 1,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 115,
+        "countryCode": "GT",
+        "name": "Banco Industrial, S.A."
+    },
+    {
+        "id": 1,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 116,
+        "countryCode": "GT",
+        "name": "Banco de Desarrollo Rural, S.A."
+    },
+    {
+        "id": 1,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 119,
+        "countryCode": "GT",
+        "name": "Banco Internacional, S.A."
+    },
+    {
+        "id": 1,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 130,
+        "countryCode": "GT",
+        "name": "Citibank NA"
+    },
+    {
+        "id": 1,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 136,
+        "countryCode": "GT",
+        "name": "Vivibanco, S.A."
+    },
+    {
+        "id": 1,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 139,
+        "countryCode": "GT",
+        "name": "Banco Ficohsa Guatemala, S.A."
+    },
+    {
+        "id": 1,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 140,
+        "countryCode": "GT",
+        "name": "Promerica, S.A."
+    },
+    {
+        "id": 1,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 141,
+        "countryCode": "GT",
+        "name": "Banco de Antigua, S.A."
+    },
+    {
+        "id": 1,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 42,
+        "countryCode": "GT",
+        "name": "BAC Guatemala"
+    },
+    {
+        "id": 1,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 144,
+        "countryCode": "GT",
+        "name": "Banco Agromercantil"
+    },
+    {
+        "id": 1,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 145,
+        "countryCode": "GT",
+        "name": "Banco  G & T Continental "
+    },
+    {
+        "id": 1,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 146,
+        "countryCode": "GT",
+        "name": "Banco de Credito"
+    },
+    {
+        "id": 1,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 147,
+        "countryCode": "GT",
+        "name": "Banco Azteca"
+    },
+];
+"Uruguay": [
+    {
+        "id": ,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 001,
+        "countryCode": "UY",
+        "name": "BROU - Banco de la República Oriental del Uruguay"
+    },
+    {
+        "id": ,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 091,
+        "countryCode": "UY",
+        "name": "Banco Hipotecario del Uruguay"
+    },
+    {
+        "id": 1,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 113,
+        "countryCode": "UY",
+        "name": "Banco ITAU"
+    },
+    {
+        "id": 1,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 128,
+        "countryCode": "UY",
+        "name": "Scotiabank"
+    },
+    {
+        "id": 1,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 137,
+        "countryCode": "UY",
+        "name": "Banco Santander"
+    },
+    {
+        "id": 1,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 153,
+        "countryCode": "UY",
+        "name": "Banco Bilbao Vizcaya Argentaria"
+    },
+    {
+        "id": 1,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 157,
+        "countryCode": "UY",
+        "name": "HSBC Bank"
+    },
+    {
+        "id": 1,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 162,
+        "countryCode": "UY",
+        "name": "Banque Heritage"
+    },
+    {
+        "id": 1,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 205,
+        "countryCode": "UY",
+        "name": "Citibank N.A. Sucursal"
+    },
+    {
+        "id": 1,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 246,
+        "countryCode": "UY",
+        "name": "Banco de la Nación Argentina"
+    },
+    {
+        "id": 1,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 61,
+        "countryCode": "UY",
+        "name": "BAPRO"
+    },
+    {
+        "id": 1,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 603,
+        "countryCode": "UY",
+        "name": "Prex"
+    },
+    {
+        "id": 1,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 999,
+        "countryCode": "UY",
+        "name": "Oca Blue"
+    },
+    {
+        "id": 1,
+        "createdAt": "0001-01-01T00:00:00Z",
+        "updatedAt": "0001-01-01T00:00:00Z",
+        "deletedAt": null,
+        "bankCode": 624,
+        "countryCode": "UY",
+        "name": "Redpagos"
+    }
+];
